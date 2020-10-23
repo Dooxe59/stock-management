eb6c7e806fb4f751ab4dbe77d343d8b4f64aec5b
Dooxe59 <anthony@ruelle.fr>
2020-10-23 22:14:40 +0200

    Add popover for explain color icon signification

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import store from './store/index.js';
 import Home from "./pages/home/Home";
 import Administration from "./pages/administration/Administration";
 import { Provider } from "react-redux";
-import { Button, IconButton } from "@chakra-ui/core";
-import { SettingsIcon } from "@chakra-ui/icons";
+import { Button, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Text } from "@chakra-ui/core";
+import { BellIcon, QuestionIcon, SettingsIcon } from "@chakra-ui/icons";
 
 import "./app.scss";
 
@@ -38,6 +38,39 @@ const App = () => {
                 size="xs" 
                 colorScheme="teal"/>
             </Link>
+            <Popover>
+              <PopoverTrigger>
+                <IconButton 
+                  className="help-button-icon"
+                  title="Aide"
+                  variant="outline"
+                  icon={<QuestionIcon />} 
+                  size="xs" 
+                  colorScheme="teal"/>
+              </PopoverTrigger>
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Aide</PopoverHeader>
+                <PopoverBody>
+                  <Text 
+                    fontSize={["xs", "sm"]}
+                    className="bell-icon-alert">
+                    { <BellIcon/> }: Date passée ou aujourd'hui
+                  </Text>
+                  <Text 
+                  fontSize={["xs", "sm"]}
+                  className="bell-icon-warning">
+                  { <BellIcon/> }: Date proche (1 à 3 jours)
+                </Text>
+                <Text 
+                  fontSize={["xs", "sm"]}
+                  className="bell-icon">
+                  { <BellIcon /> }: Reste plus de 3 jours
+                </Text>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </div>
           <Switch>
             <Route path="/administration">
