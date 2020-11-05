e2f22914b9f089327c5d51caac1c5c2de6f61178
Dooxe59 <anthony@ruelle.fr>
2020-11-05 09:29:07 +0100

    Fix build failed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
 import * as serviceWorker from './serviceWorker';
 import { ChakraProvider } from "@chakra-ui/core";
-import { registerLocale } from  "react-datepicker";
 import store from './store/index.js';
 import fr from 'date-fns/locale/fr';
-
 import moment from "moment";
 import "moment/locale/fr";
 import { Provider } from 'react-redux';
 import AuthProvider from "./components/authProvider/AuthProvider";
 
-registerLocale('fr', fr);
+import './index.scss';
+
 moment.locale("fr");
 
 
